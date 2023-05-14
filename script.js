253f980eb5695cbb41bd8debd868a3c428cf2937
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-05-14 15:49:54 +0300

    Made a div grid inside js, added flexbox, began trying to set on click event

--- script.js
+++ script.js
@@ -12,9 +12,63 @@ This is a perfect opportunity to apply what you have learned about flexbox.
 
 Be careful with borders and margins, as they can adjust the size of the squares! */
 
-//create 16 divs grid with for loop
-for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    document.getElementById('gridContainer').appendChild(square);
-    square.style.cssText = 'background:grey; padding:5px; margin:5px;';
+//create 16 divs with for loop
+//first row
+const rowOne = document.createElement('div');
+    rowOne.setAttribute('id', 'rowOne');
+    document.getElementById('gridContainer').appendChild(rowOne);
+    rowOne.style.display = 'flex';
+for (let i = 0; i < 4; i++) {
+    const squareOne = document.createElement('div');
+    squareOne.setAttribute('id', 'squareOne');
+    document.getElementById('rowOne').appendChild(squareOne);
+    squareOne.style.cssText = 'background:lightgrey; padding:50px; margin:5px;';
+    squareOne.style.flex = 1;
 }
+
+//second row
+const rowTwo = document.createElement('div');
+    rowTwo.setAttribute('id', 'rowTwo');
+    document.getElementById('gridContainer').appendChild(rowTwo);
+    rowTwo.style.display = 'flex';
+for (let i = 0; i < 4; i++) {
+    const squareTwo = document.createElement('div');
+    document.getElementById('rowTwo').appendChild(squareTwo);
+    squareTwo.style.cssText = 'background:blue; padding:50px; margin:5px;';
+    squareTwo.style.flex = 1;
+}
+
+//third row
+const rowThree = document.createElement('div');
+    rowThree.setAttribute('id', 'rowThree');
+    document.getElementById('gridContainer').appendChild(rowThree);
+    rowThree.style.display = 'flex';
+for (let i = 0; i < 4; i++) {
+    const squareThree = document.createElement('div');
+    document.getElementById('rowThree').appendChild(squareThree);
+    squareThree.style.cssText = 'background:yellow; padding:50px; margin:5px;';
+    squareThree.style.flex = 1;
+}
+
+//fourth row
+const rowFour = document.createElement('div');
+    rowFour.setAttribute('id', 'rowFour');
+    document.getElementById('gridContainer').appendChild(rowFour);
+    rowFour.style.display = 'flex';
+for (let i = 0; i < 4; i++) {
+    const squareFour = document.createElement('div');
+    document.getElementById('rowFour').appendChild(squareFour);
+    squareFour.style.cssText = 'background:green; padding:50px; margin:5px;';
+    squareFour.style.flex = 1;
+}
+
+//create square grid
+const grid = document.getElementById('gridContainer');
+    grid.style.cssText = 'height: 500px; width: 500px;';
+
+//create click effect
+let clickOne = document.getElementById('squareOne');
+    clickOne.addEventListener('click', () => {
+        document.getElementById('squareOne').style = 'background: pink';
+});
+    
