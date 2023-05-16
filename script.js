517cd1480426f3739faf29875a5af2611c987a1e
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-05-16 12:52:13 +0300

    working hover effect on all divs

--- script.js
+++ script.js
@@ -20,12 +20,11 @@ const rowOne = document.createElement('div');
     rowOne.style.display = 'flex';
 for (let i = 0; i < 4; i++) {
     const squareOne = document.createElement('div');
-    squareOne.setAttribute('id', 'squareOne');
+    squareOne.classList.toggle('square_One');
     document.getElementById('rowOne').appendChild(squareOne);
-    squareOne.style.cssText = 'background:lightgrey; padding:50px; margin:5px;';
+    squareOne.style.cssText = 'background:lightgrey; padding:50px;';
     squareOne.style.flex = 1;
 }
-
 //second row
 const rowTwo = document.createElement('div');
     rowTwo.setAttribute('id', 'rowTwo');
@@ -33,11 +32,11 @@ const rowTwo = document.createElement('div');
     rowTwo.style.display = 'flex';
 for (let i = 0; i < 4; i++) {
     const squareTwo = document.createElement('div');
+    squareTwo.classList.toggle('square_Two');
     document.getElementById('rowTwo').appendChild(squareTwo);
-    squareTwo.style.cssText = 'background:blue; padding:50px; margin:5px;';
+    squareTwo.style.cssText = 'background:lightgrey; padding:50px;';
     squareTwo.style.flex = 1;
 }
-
 //third row
 const rowThree = document.createElement('div');
     rowThree.setAttribute('id', 'rowThree');
@@ -45,11 +44,11 @@ const rowThree = document.createElement('div');
     rowThree.style.display = 'flex';
 for (let i = 0; i < 4; i++) {
     const squareThree = document.createElement('div');
+    squareThree.classList.toggle('square_Three');
     document.getElementById('rowThree').appendChild(squareThree);
-    squareThree.style.cssText = 'background:yellow; padding:50px; margin:5px;';
+    squareThree.style.cssText = 'background:lightgrey; padding:50px;';
     squareThree.style.flex = 1;
 }
-
 //fourth row
 const rowFour = document.createElement('div');
     rowFour.setAttribute('id', 'rowFour');
@@ -57,18 +56,55 @@ const rowFour = document.createElement('div');
     rowFour.style.display = 'flex';
 for (let i = 0; i < 4; i++) {
     const squareFour = document.createElement('div');
+    squareFour.classList.toggle('square_Four');
     document.getElementById('rowFour').appendChild(squareFour);
-    squareFour.style.cssText = 'background:green; padding:50px; margin:5px;';
+    squareFour.style.cssText = 'background:lightgrey; padding:50px;';
     squareFour.style.flex = 1;
 }
 
-//create square grid
+//create square grid container
 const grid = document.getElementById('gridContainer');
-    grid.style.cssText = 'height: 500px; width: 500px;';
+    grid.style.cssText = 'max-height: 400px; max-width: 400px; margin-left: auto; margin-right: auto;';
+    grid.style.boxShadow = '2px 2px 2px 2px grey';
+    grid.style.alignSelf = 'center';
 
-//create click effect
-let clickOne = document.getElementById('squareOne');
-    clickOne.addEventListener('click', () => {
-        document.getElementById('squareOne').style = 'background: pink';
+//create hover effect
+//first row
+let getSquareOne = document.getElementsByClassName('square_One');
+/*console.log(getSquareOne);  returns array like object */
+//loop through all array elements 
+for (let i = 0; i < getSquareOne.length; i++) {
+getSquareOne[i].addEventListener('mouseover', () => {
+    getSquareOne[i].style = 'background: black; padding:50px;';
+    getSquareOne[i].style.flex = 1; 
 });
+};
+//second row
+let getSquareTwo = document.getElementsByClassName('square_Two'); 
+for (let i = 0; i < getSquareTwo.length; i++) {
+    getSquareTwo[i].addEventListener('mouseover', () => {
+        getSquareTwo[i].style = 'background: black; padding:50px;';
+        getSquareTwo[i].style.flex = 1; 
+    });
+};
+//third row
+let getSquareThree = document.getElementsByClassName('square_Three');
+for (let i = 0; i < getSquareThree.length; i++) {
+    getSquareThree[i].addEventListener('mouseover', () => {
+        getSquareThree[i].style = 'background: black; padding:50px;';
+        getSquareThree[i].style.flex = 1; 
+    });
+};
+
+//fourth row
+let getSquareFour = document.getElementsByClassName('square_Four');
+for (let i = 0; i < getSquareFour.length; i++) {
+    getSquareFour[i].addEventListener('mouseover', () => {
+        getSquareFour[i].style = 'background: black; padding:50px;';
+        getSquareFour[i].style.flex = 1; 
+    });
+};
+
+
+
     
